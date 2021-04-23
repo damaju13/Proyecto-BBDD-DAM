bba2441c58532b560905dee27044c6e5aef2cbe1
damaju13 <81973179+damaju13@users.noreply.github.com>
2021-04-23 16:52:47 +0200

    Delete Plantilla de DROP.sql

--- Plantilla de DROP.sql
+++
@@ -1,7 +0,0 @@
---Plantilla para eliminar todos los procedimientos y funciones. Los disparadores se borran por si solos al eliminar las tablas
-DROP PROCEDURE muestraEmpleado;
-DROP PROCEDURE muestraCliente;
-DROP FUNCTION esPrecio;
-DROP FUNCTION esJefe;
-
-COMMIT;
